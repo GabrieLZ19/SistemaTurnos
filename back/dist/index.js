bf9ed159417d3b529d00d5e88599f714cfb926ad
Gabriel Lazo <gabriellazo48@gmail.com>
2024-04-03 00:08:35 -0300

    integracion con Typescript agregada

---
+++ back/dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+const num1 = 10;
+const num2 = 15;
+function sumar(num1, num2) {
+    return num1 + num2;
+}
+console.log(sumar(num1, num2));
---
+++ back/src/index.ts
@@ -0,0 +1,8 @@
+const num1: number = 10;
+const num2: number = 15;
+
+function sumar(num1: number, num2: number) {
+  return num1 + num2;
+}
+
+console.log(sumar(num1, num2));
