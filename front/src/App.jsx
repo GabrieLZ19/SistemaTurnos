26b006cd277d1b672110cd81caa788ae093c1981
Gabriel Lazo <gabriellazo48@gmail.com>
2024-04-10 11:54:16 -0300

    stylos modificados

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,8 +1,10 @@
 import Home from "./views/Home/Home";
+import NavBar from "./components/NavBar/NavBar";
 import "./styles/App.module.css";
 function App() {
   return (
     <>
+      <NavBar />
       <Home />
     </>
   );
--- front/src/views/Home/Home.jsx
+++ front/src/views/Home/Home.jsx
@@ -1,13 +1,9 @@
-import NavBar from "../../components/NavBar/NavBar";
 import styles from "../../styles/Home.module.css";
 const Home = () => {
   return (
-    <>
-      <NavBar />
-      <div className={styles.containerMain}>
-        <h1> holaa</h1>
-      </div>
-    </>
+    <div className={styles.containerMain}>
+      <h1> holaa</h1>
+    </div>
   );
 };
 
