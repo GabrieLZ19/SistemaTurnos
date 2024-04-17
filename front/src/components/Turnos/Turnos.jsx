08981a0544bf6ef8e84d1cdc0815fac6249f8072
Gabriel Lazo <gabriellazo48@gmail.com>
2024-04-17 11:13:26 -0300

    funcion cancelar mejorada y alerta agregada

--- front/src/components/Turnos/Turnos.jsx
+++ front/src/components/Turnos/Turnos.jsx
@@ -1,31 +1,70 @@
-import styles from "./Turnos.module.css";
-import axios from "axios";
 import { useState } from "react";
+import axios from "axios";
+import Swal from "sweetalert2";
+import { useNavigate } from "react-router-dom";
+import styles from "./Turnos.module.css";
 
 const Turnos = ({ turnos: { id, time, date, description, status } }) => {
-  const [turns, setMyTurns] = useState([]);
+  const navigate = useNavigate();
+  const [appointmentStatus, setAppointmentStatus] = useState(status);
 
   const handleOnClick = async () => {
-    try {
-      // Enviar solicitud para cancelar el turno
-      await axios.put(`http://localhost:3000/appointments/cancel/${id}`);
+    Swal.fire({
+      title: "¿Estás seguro?",
+      text: "¡Este cambio no se puede revertir!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Sí, cancelar turno",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: "Cancelando turno...",
+          text: "Por favor espere...",
+          icon: "info",
+          allowOutsideClick: false,
+          allowEscapeKey: false,
+          showConfirmButton: false,
+        });
 
-      // Obtener los turnos actualizados después de la cancelación
-      const response = await axios.get("http://localhost:3000/appointments");
+        try {
+          // Realiza la solicitud para cancelar el turno
+          await axios.put(`http://localhost:3000/appointments/cancel/${id}`);
 
-      // Actualizar el estado local de los turnos con los turnos actualizados
-      setMyTurns(response.data);
-    } catch (error) {
-      console.error("Error al cancelar el turno:", error);
-    }
+          // Actualiza el estado del turno en la UI
+          setAppointmentStatus("cancelled");
+
+          Swal.fire({
+            title: "¡Turno Cancelado!",
+            text: "Tu turno fue cancelado",
+            icon: "success",
+          });
+
+          // Navega a la página de turnos después de la cancelación
+          navigate("/appointments");
+        } catch (error) {
+          // Si hay un error, muestra un mensaje de error y restaura el estado del turno
+          setAppointmentStatus(status);
+          Swal.fire({
+            title: "Error!",
+            text: "Hubo un error al cancelar el turno",
+            icon: "error",
+          });
+        }
+      }
+    });
   };
+
   return (
     <div className={styles.containerTurnos}>
       <h4>{date} </h4>
       <h4>{time} </h4>
       <h4> {description}</h4>
-      <h4 name="status">{status} </h4>
-      <button onClick={handleOnClick}>Cancelar</button>
+      <h4>{appointmentStatus} </h4>
+      {appointmentStatus === "active" && (
+        <button onClick={handleOnClick}>Cancelar Turno</button>
+      )}
     </div>
   );
 };
