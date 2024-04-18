bddccfa5f63591a5bcbf2e960cd40427044c3280
Gabriel Lazo <gabriellazo48@gmail.com>
2024-04-17 23:45:15 -0300

    restriccion de navBar eliminada

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -6,18 +6,13 @@ import LoginPage from "./views/Login/Login.jsx";
 import RegisterPage from "./views/Register/Register.jsx";
 import CreateTurnPage from "./views/MisTurnos/CrearTurnos.jsx";
 import ErrorPage from "./views/Error/Error.jsx";
-import { useLocation } from "react-router-dom";
 
 import { Routes, Route } from "react-router-dom";
 
 function App() {
-  const location = useLocation();
   return (
     <>
-      {location.pathname !== "/login" && location.pathname !== "/register" && (
-        <NavBar />
-      )}
-
+      <NavBar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/appointments" element={<TurnosPage />} />
